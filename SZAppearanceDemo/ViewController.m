93b9a7f4c93bebe484a6675dbe0c0ada7fffae5a
Song Zhou <zhousong1993@gmail.com>
2018-12-23 19:40:26 +0800

    initial commit

--- SZAppearanceDemo/ViewController.m
+++ SZAppearanceDemo/ViewController.m
@@ -8,16 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewControllerView : UIView
+
+@property (nonatomic) UITableView *tableView;
+
+@end
+
+@implementation ViewControllerView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor whiteColor];
+        _tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
+        [self addSubview:_tableView];
+        
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    self.tableView.frame = self.bounds;
+}
+
+@end
+
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic) ViewControllerView *view;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+@dynamic view;
+- (void)loadView {
+    self.view = [ViewControllerView new];
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.tableView.dataSource = self;
+    self.view.tableView.delegate = self;
+    
+    [self.view.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"c"];
+}
 
+#pragma mark - TableView
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"c"];
+    
+    cell.textLabel.text = @(indexPath.row).stringValue;
+    
+    return cell;
+}
 @end

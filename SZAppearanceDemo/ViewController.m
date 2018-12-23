2852201df9c586aed920a5929f626d38a1548a78
Song Zhou <zhousong1993@gmail.com>
2018-12-23 20:34:32 +0800

    up

--- SZAppearanceDemo/ViewController.m
+++ SZAppearanceDemo/ViewController.m
@@ -8,9 +8,37 @@
 
 #import "ViewController.h"
 
+@interface SZTableView : UITableView
+
+@end
+
+@implementation SZTableView
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor {
+    [super setBackgroundColor:backgroundColor];
+    
+}
+
+@end
+
+@interface SZTableViewCell : UITableViewCell
+
+@property (nonatomic) UIColor *contentBackgroundColor UI_APPEARANCE_SELECTOR;
+
+@end
+
+
+@implementation SZTableViewCell
+
+- (void)setContentBackgroundColor:(UIColor *)contentBackgroundColor {
+    self.contentView.backgroundColor = contentBackgroundColor;
+}
+
+@end
+
 @interface ViewControllerView : UIView
 
-@property (nonatomic) UITableView *tableView;
+@property (nonatomic) SZTableView *tableView;
 
 @end
 
@@ -20,7 +48,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
-        _tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
+        _tableView = [[SZTableView alloc] initWithFrame:frame style:UITableViewStylePlain];
         [self addSubview:_tableView];
         
     }
@@ -31,6 +59,10 @@
     [super layoutSubviews];
     
     self.tableView.frame = self.bounds;
+}
+
+- (void)willMoveToSuperview:(UIView *)newSuperview {
+    
 }
 
 @end
@@ -54,7 +86,7 @@
     self.view.tableView.dataSource = self;
     self.view.tableView.delegate = self;
     
-    [self.view.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"c"];
+    [self.view.tableView registerClass:[SZTableViewCell class] forCellReuseIdentifier:@"c"];
 }
 
 #pragma mark - TableView
@@ -63,10 +95,49 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"c"];
+    SZTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"c"];
     
     cell.textLabel.text = @(indexPath.row).stringValue;
     
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self _setupAppearance:indexPath.row != 0];
+}
+
+- (void)_setupAppearance:(BOOL)dark {
+    
+    UIColor *darkGrayColor = UIColor.darkGrayColor;
+    UIColor *whiteColor = UIColor.whiteColor;
+    
+    UIColor *bgColor;
+    UIColor *labelColor;
+    if (dark) {
+        bgColor = darkGrayColor;
+        labelColor = whiteColor;
+    } else {
+        bgColor = whiteColor;
+        labelColor = darkGrayColor;
+    }
+    
+    // Appearances
+    UITableView *tableViewAppearance = [UITableView appearance];
+    SZTableViewCell *tableViewCellAppearance = [SZTableViewCell appearance];
+    UILabel *labelInTableViewCell = [UILabel appearanceWhenContainedInInstancesOfClasses:@[SZTableViewCell.class]];
+    
+    tableViewAppearance.backgroundColor = bgColor;
+    tableViewCellAppearance.contentBackgroundColor = bgColor;
+    labelInTableViewCell.textColor = labelColor;
+
+    
+    /// update appearance
+    NSArray *windows = UIApplication.sharedApplication.windows;
+    for (UIWindow *window in windows) {
+        for (UIView *view in window.subviews) {
+            [view removeFromSuperview];
+            [window addSubview:view];
+        }
+    }
 }
 @end
